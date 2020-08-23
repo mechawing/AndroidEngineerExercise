93bb29f917aca42de2efe5b42ad150ec2a5d770e
Jackson <jacksonhopknins92@gmail.com>
2020-08-23 14:31:52 -0400

    update1

--- app/src/main/java/com/example/androidengineerexercise/MainActivity.kt
+++ app/src/main/java/com/example/androidengineerexercise/MainActivity.kt
@@ -4,30 +4,46 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
 
+    val editTxtEmail: EditText = findViewById<EditText>(R.id.emailInput)
+    val editTxtPassword: EditText = findViewById<EditText>(R.id.enterPassword)
+    val editTxtConfirmPassword: EditText = findViewById<EditText>(R.id.confirmPassword)
+
+    val nextButton: ImageButton = findViewById<ImageButton>(R.id.imageButton)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val emailAddress = editTxtEmail.text.toString()
+        val password = editTxtPassword.text.toString()
+        val confirmPassword = editTxtConfirmPassword.text.toString()
+
+        nextButton.setOnClickListener() {
+            setEmailAddress(emailAddress)
+            setPassword(password)
+            confirmPassword(confirmPassword)
+        }
     }
 
     fun createAccount(view: View) {
         setContentView(R.layout.second_activity)
     }
 
-    fun setEmailAddress(view: View) {
-        val editTxt =findViewById<EditText>(R.id.emailInput)
-        val emailAddress = editTxt.text.toString()
+    fun setEmailAddress(view: String) {
+
     }
 
-    fun setPassword(view: View) {
-        val editTxt =findViewById<EditText>(R.id.emailInput)
-        val emailAddress = editTxt.text.toString()
+    fun setPassword(view: String) {
+
     }
 
+    fun confirmPassword(view: String) {
 
+    }
 
     fun goBack(view: View) {
         setContentView(R.layout.activity_main)
